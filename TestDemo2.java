dd22f801864912424c6f5b68383215128941a8ff
rain-point <619342747@qq.com>
2021-03-15 22:22:04 +0800

    编程代码练习

---
+++ TestDemo2.java
@@ -0,0 +1,21 @@
+
+public class TestDemo{
+	public static void main(String[] args) {
+		boolean b=true;
+		System.out.println(!b);
+		int a=10;
+		int c=20;
+		System.out.println(!(a<20));
+	}
+	public static void main2(String[] args) {
+		System.out.println(10%3);
+		System.out.println(-10%3);
+		System.out.println(10%-3);
+		System.out.println(-10%-3);
+	}
+	public static void main1(String[] args) {
+		System.out.println(5/2);
+		System.out.println(5.0/2);
+		System.out.println((float)5/2);
+    }
+}

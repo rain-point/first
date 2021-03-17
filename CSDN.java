8bee8763d6337ebbee235d06007fb01f74fce1e9
rain-point <619342747@qq.com>
2021-03-17 23:22:22 +0800

    编程代码练习

---
+++ CSDN.java
@@ -0,0 +1,38 @@
+ public class CSDN{
+ 	public static void main(String[] args) {
+ 		short sh = 12;
+ 	}
+ 	public static void main6(String[] args) {
+ 		byte b = 12;
+ 		byte c = 21;
+ 		System.out.println(b+" "+c);
+ 	}
+ 	public static void main5(String[] args) {
+ 		char ch = 'a';
+ 		System.out.println(ch);
+ 		char ch2 = 97;
+ 		System.out.println(ch2);
+ 	}
+ 	public static void main4(String[] args) {
+ 		float f = 12.3;
+ 		System.out.println(f);
+ 	}
+ 	public static void main3(String[] args) {
+ 		//双精度浮点型
+ 		double d = 12.5;
+ 		System.out.println(d);
+ 	}
+ 	public static void main2(String[] args) {
+ 		Long a = 10L;
+ 		System.out.println(a);
+        System.out.println("a的值是:"+a);//打印汉字用引号括起来然后用加号拼接
+ 	    int b = 10;
+ 	    int c = 20;
+        System.out.println("hhh"+a+b);
+ 	}
+ 	public static void main1(String[] args) {
+ 		int a = 10;
+ 		System.out.println(Integer.MAX.VALUE);
+ 		System.out.println(Integer.MIN.VALUE);
+ 	}
+ }

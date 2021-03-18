5aadae4c97f123623bf4c4edac52a8da0d81e1fc
rain-point <619342747@qq.com>
2021-03-18 21:48:19 +0800

    Java

---
+++ Last.java
@@ -0,0 +1,9 @@
+ 
+public class Last{
+	public static void main(String[] args) {
+		System.out.println(5/2);
+		System.out.println(5.0/2);
+		System.out.println((float)5/2);
+		System.out.println(float)(5/2);
+	}
+}

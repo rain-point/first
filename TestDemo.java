21cd0d7ceade8c76540a583cd12f04e3fec308b4
rain-point <619342747@qq.com>
2021-03-26 21:59:42 +0800

    java

--- TestDemo.java
+++ TestDemo.java
@@ -1,37 +1,43 @@
+import java.util.Scanner;
 
-public class TestDemo{
+public class testdemo {
 
-	//数组代表的是什么
-	public static void main(String[] args) {
-		double d = 12.5;//默认双精度类型
-		System.out.println(d);
-		System.out.println(Double.MAX_VALUE);
-		System.out.println(Double.MIN_VALUE);
+    public static int maxNum(int num1,int num2){
+        return num1>num2 ? num1:num2;
+    }
 
-		int a=1;
-		int b=2;
-		System.out.println(a/b);//a和b都是整型，不能保存小数
+    public static void main(String[] args) {
+         int a=10;
+         int b=20;
+         int sum=maxNum(a,b);
 
-		double num=1.1;
-		System.out.println(num*num);
-	}
-	public static void main2(String[] args) {
-		for(int i=0; i < args.legth; i++){
-			System.out.println(args[i]);
-
-		}
-		System.out.println("kaixin"); 
-	}
-	public static void main1(String[] args){
-
-		Long a=10L;
-		System.out.println(a);
-		System.out.println("最大值:"+Long.MAX_VALUE);
-		System.out.println("最小值:"+Long.MIN_VALUE);//拼接
-		int b=10;
-		int c=20;
-		System.out.println(b+c);
-		//注意：任何类型的数据和字符串进行拼接结果就是字符串
-		System.out.println("hhh"+b+c);
-	}
+    }
+    public static void main3(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        int ret = maxNum(n1,n2);
+        System.out.println(ret);
+    }
+    public static void main2(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+        int count = 0;
+        while(n!=0){
+            n=n&(n-1);
+            count++;
+        }
+        System.out.println(count);
+    }
+    public static void main1(String[] args) {//psvm
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int count = 0;
+        for(int i = 0;i<32;i++){
+            if(((n>>i)&1)==1){
+                count++;
+            }
+            System.out.println("count:"+count);
+        }
+    }
 }
--- TestDemo3.java
+++ TestDemo3.java
@@ -1,50 +1,44 @@
-import java.util.Scanner;
-public class testdemo2{
-	public static void main(String[] args) {
-		int i=0;
-		int ret=1
-		int sum=0;
-		for(i=1;i<=5;i++){
-			ret*=i;
-			sum=ret+sum;
-		}
-	}
-	//	public static void main4(String[] args) {
-		int a=10;
-		switch(a){
-			case 1:
-			System.out.println("1");
-			break;
-			case 2:
-			System.out.println("2");
-			break;
-            case 3:
-			System.out.println("3");
-			break;
-		}
-	}
-	public static void main3(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		while(scan.hasNext()){
-		int year = scan.nextInt();
-		if((year%4==0 && year%100!=0)||(year%400==0)){
-	    System.out.println("闰年");}
-	else{
-		System.out.println("不是闰年");
-	}
-	}
-}
-	public static void main2(String[] args) {
-		int a = 10;
-		if(a==10){
-			System.out.println("hello");
-		}
-	}
-	public static void main1(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		System.out.println(a);
-		String str = scan.nextLine();
-		System.out.println(str);
-	}
+public class testdemo3 {
+    //方法的重载
+    //1.函数名可以相同
+    //返回值不做要求
+    //参数列表不同（参数的个数或参数的类型不同）
+    //在同一个类当中
+    public static int fac(int n){
+        if(n==1){
+            return 1;
+        }
+        return n*fac(n:n-1);
+    }
+    public static void main(String[] args) {
+        System.out.println(fac(5));
+
+    }
+    public static void main3(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(sum(a,b));
+        double d1 = 19.1;
+        double d2 = 19.3;
+        System.out.println(sum(d1,d2));
+
+    }
+    public static int sum(int a,int b){
+        return a+b;
+    }
+    public static double sum(double a,double b){
+        return a+b;
+    }
+    public static void swap(int x,int y) {
+        int tmp=0;
+        tmp = x;
+        x = y;
+        y = tmp;
+    }
+    public static void main2(String[] args) {
+        int a = 10;
+        int b = 20;
+        swap(a,b);
+        System.out.println(a+" "+b);
+    }
 }

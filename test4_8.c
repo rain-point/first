94f40e8160459d4a174eae1a3df2d8bf4a84241c
rain-point <619342747@qq.com>
2021-04-08 23:33:33 +0800

    编程代码练习

---
+++ test4_8.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1m
+#include<stdio.h>
+#include"SeqList.h"
+void TestSeqList1()
+{
+	SeqList s;
+	SeqListInt(&s);
+
+
+	SeqListDestory(&s)
+}
+int main()
+{
+	TestSeqList1();
+	return 0;
+}

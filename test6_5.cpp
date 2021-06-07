392cc2e7c8516bc68ae233301c639665a7997d0c
rain-point <619342747@qq.com>
2021-06-07 23:46:33 +0800

    编程代码练习

---
+++ test6_5.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+//class Date
+//{
+//public:
+//	bool operator==(const Date&d)const
+//	{
+//		return (_year == d._year)
+//			&& (_month == d._month)
+//			&& (_day == d._day);
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+class Date
+{
+public:
+	Date(int year=0,int month-1)
+};
